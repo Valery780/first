892f0e5fb950e4ea77014d5e459b6499466f8cf9
Valery780 <70340887+Valery780@users.noreply.github.com>
2020-09-01 21:44:38 +0300

    Add files via upload

---
+++ first.py
@@ -0,0 +1,8 @@
+a= ("\\a Bell (alert)")
+b= ("\\b Backspace")
+c= ("\\n New line")
+d= ("\\t Horizontal tab")
+e= ("\\\ Backslash")
+f= ("\\\" Double quotation mark")
+g= ("\\' Single quotation mark")
+print(a,"\n",b,"\n",c,"\n",d,"\n",f,"\n",g)
